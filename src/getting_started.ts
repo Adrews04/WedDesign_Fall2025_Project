17b82ba2e0830f51d1a46d32da3a77223f930ae3
Sadisa <sadisapatvatibl@hotmail.com>
2025-12-13 15:35:17 -0600

    Small fixes

--- src/getting_started.ts
+++ src/getting_started.ts
@@ -1,13 +1,7 @@
-//interface Window {
-//    ts: typeof import("typescript");
-//}
-//const localTs = window.ts;
-
-declare const ts: any;
-
-const localTs = ts;
-
-
+interface Window {
+    ts: typeof import("typescript");
+}
+const localTs = window.ts;
 
 const codeInput = document.getElementById("codeInput") as HTMLTextAreaElement | null;
 const runBtn = document.getElementById("runBtn") as HTMLButtonElement | null;
